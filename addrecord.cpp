9c9628f6407db152fe225fc1743f63a1beee05bc
Ricky-Lin <47148048+Ricky-Lin@users.noreply.github.com>
2019-04-16 23:03:41 +0800

    Update addrecord.cpp

--- addrecord.cpp
+++ addrecord.cpp
@@ -1,4 +1,15 @@
 include<iostream>
 using namespace std;
+struct record{
+  string accounttype;
+  string moneytype;
+  double money;
+};
 mian(){
+  int const max=100;
+  record add[max];
+  
+  
+  
+  
   
