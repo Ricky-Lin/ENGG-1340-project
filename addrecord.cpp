e98148fb99a47033ae4246763da5892e63f8cd09
Ricky-Lin <47148048+Ricky-Lin@users.noreply.github.com>
2019-04-23 21:53:05 +0800

    Update addrecord.cpp

--- addrecord.cpp
+++ addrecord.cpp
@@ -7,7 +7,7 @@ struct record{
   string date;
   double money;
 };
-mian(){
+main(){
   int const max=100;
   //Use dynamic memory to create an array of records in order to delete the records conveninently.
   record * a=new record [max];
@@ -15,40 +15,50 @@ mian(){
   int i=0,j=0;
   string x;
   cout<<"What do you want to do?"<<endl;
-  cin>>x;
-  if (x=="INCOME"){
-    for (i;i<max;i++){
-      //Get user's input of the amonut of money, the account type, the type of money and date of income records.
-      cin>>a[i].money;
-      cin>>a[i].moneytype;
-      cin>>a[i].accounttype;
-      cin>>a[i].date;
-      cin>>x;
-      if (x=="FINISH"){
-        i=i;
-        break;
-      }  
-    }
-  }
-  if (x=="EXPENSE"){
-    for (j;j<max;j++){
-      //Get user's input of the amonut of money, the account type, the type of money and date of expense records.
-      cin>>b[j].money;
-      cin>>b[j].moneytype;
-      cin>>b[j].accounttype;
-      cin>>b[j].date;
-      cin>>x;
-      if (x=="FINISH"){
-        j=j;
-        break;
-      }  
-    }
-  }
-  if (x=="DELETEA"){
-    delete [] a;
-  } 
-  if (x=="DELETEB"){
-    delete [] b;
-  } 
-  cin>>x;
-}
+  while(true){
+   cin>>x;
+   if (x=="INCOME"){
+     for (i;i<max;i++){
+       //Get user's input of the amonut of money, the account type, the type of money and date of income records.
+       cin>>a[i].money;
+       cin>>a[i].moneytype;
+       cin>>a[i].accounttype;
+       cin>>a[i].date;
+       cout<<"CONTINUE or FINISH"<<endl;
+       cin>>x;
+       if (x=="FINISH"){
+         i=i;
+         break;
+       }  
+     }
+   }
+   cin>>x; 
+   if (x=="EXPENSE"){
+     for (j;j<max;j++){
+       //Get user's input of the amonut of money, the account type, the type of money and date of expense records.
+       cin>>b[j].money;
+       cin>>b[j].moneytype;
+       cin>>b[j].accounttype;
+       cin>>b[j].date;
+       cout<<"CONTINUE or FINISH"<<endl;
+       cin>>x;
+       if (x=="FINISH"){
+         j=j;
+         break;
+       }  
+     }
+   }
+   cin>>x;
+   if (x=="DELETEA"){
+     delete [] a;
+   }
+   cin>>x;
+   if (x=="DELETEB"){
+     delete [] b;
+   } 
+   cin>>x;
+   if (x=="DONE"){
+     break;
+   } 
+ }
+} 
