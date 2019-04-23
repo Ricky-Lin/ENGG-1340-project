50df370686ec6e906cb668dd2c35b782a7719df0
Ricky-Lin <47148048+Ricky-Lin@users.noreply.github.com>
2019-04-23 20:12:12 +0800

    Update view income.cpp

--- view income.cpp
+++ view income.cpp
@@ -1,1 +1,24 @@
-void viewincome()
+void viewincome(record * a[],max){
+  int sum1=0,sum2=0,sum3=0;
+  string type;
+  cin>>type;
+  for(int i=0;i<max;i++){
+    if (a[i].moneytype==type){
+      sum1+=a[i].money;
+    }
+  } 
+  cout<<"You have got "<<sum1<<" from "<<type<<endl;
+  for(int i=0;i<max;i++){
+    if (a[i].account==type){
+      sum2+=a[i].money;
+    }
+  }
+  cout<<"You have "<<sum2<<" in terms of "<<type<<endl;
+  for(int i=0;i<max;i++){
+    if (a[i].date==type){
+      sum3+=a[i].money;
+    }
+  }
+  cout<<"You have reveived "<<sum3<<" on "<<type<<endl;
+    
+}
