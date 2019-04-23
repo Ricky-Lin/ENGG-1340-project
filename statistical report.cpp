aeac36e59374a39445f1b7e755282e8273deafce
Ricky-Lin <47148048+Ricky-Lin@users.noreply.github.com>
2019-04-23 21:24:08 +0800

    Update statistical report.cpp

--- statistical report.cpp
+++ statistical report.cpp
@@ -1,4 +1,13 @@
-void report(record * a, record * b,num){
+//Print user's monthly incomes, expensed and percentage of particular expenses or incomes.
+void report(record * a, record * b,num1,num2){
   double t1=0,t2=0
-  for (i=0;i<max  
+  for (int i=0;i<num1;i++){
+    t1+=a[i].money;
+  } 
+  cout<<"Your monthly incomes are "<<t1<<endl;
+  for (int j=0;j<num2;j++){
+    t2+=b[j].money;
+  }
+  cout<<"Your monthly expenses are "<<t2<<endl;
+  
 }
