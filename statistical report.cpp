aa9c22b4ce3ab5b5e9e9fcf8478c776782c4ffc5
Ricky-Lin <47148048+Ricky-Lin@users.noreply.github.com>
2019-04-23 21:46:32 +0800

    Update statistical report.cpp

--- statistical report.cpp
+++ statistical report.cpp
@@ -1,13 +1,30 @@
 //Print user's monthly incomes, expensed and percentage of particular expenses or incomes.
 void report(record * a, record * b,num1,num2){
-  double t1=0,t2=0
-  for (int i=0;i<num1;i++){
+  double t1=0,t2=0,t3=0,t4=0;
+  string x,y;
+  for (int i=0;i<=num1;i++){
     t1+=a[i].money;
   } 
   cout<<"Your monthly incomes are "<<t1<<endl;
-  for (int j=0;j<num2;j++){
+  for (int j=0;j<=num2;j++){
     t2+=b[j].money;
   }
   cout<<"Your monthly expenses are "<<t2<<endl;
-  
+  cout<<"The percentage of what incomes do ypu want to check?"<<endl;
+  cin>>x;
+  for (int i=0;i<=num1;i++){
+    if (a[i].moneytype==x){
+      t3+=a[i].money;
+    }  
+  }
+  cout<<"The percentage of the income"<<x<<" is "<<fixed<<setprecision(1)<<t3/t1*100<<"%."<<endl;
+    
+  cout<<"The percentage of what expenses do ypu want to check?"<<endl;
+  cin>>y;
+  for (int j=0;j<=num2;j++){
+    if (b[j].moneytype==y){
+      t4+=b[j].money;
+    }  
+  }
+ cout<<"The percentage of the expense"<<x<<" is "<<fixed<<setprecision(1)<<t4/t2*100<<"%."<<endl;
 }
