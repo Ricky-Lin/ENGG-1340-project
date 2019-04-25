fc096d0af0f2fe992ab74da53774e1e297bd696e
Ricky-Lin <47148048+Ricky-Lin@users.noreply.github.com>
2019-04-25 21:57:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,14 +15,19 @@ main(){
   int i=0,j=0;
   string x;
   cout<<"What do you want to do?"<<endl;
+  cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
+  cin>>x;
   while(true){
-   cin>>x;
    if (x=="INCOME"){
      for (i;i<max;i++){
        //Get user's input of the amonut of money, the account type, the type of money and date of income records.
+       cout<<"Please enter the amount of money."<<endl;
        cin>>a[i].money;
+       cout<<"Please enter the type of money."<<endl;
        cin>>a[i].moneytype;
+       cout<<"Please enter the account type."<<endl;
        cin>>a[i].accounttype;
+       cout<<"Please enter the date."<<endl;
        cin>>a[i].date;
        cout<<"CONTINUE or FINISH"<<endl;
        cin>>x;
@@ -32,14 +37,19 @@ main(){
        }  
      }
    cout<<"What do you want to do?"<<endl;
+   cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
    cin>>x; 
    }
    if (x=="EXPENSE"){
      for (j;j<max;j++){
        //Get user's input of the amonut of money, the account type, the type of money and date of expense records.
+       cout<<"Please enter the amount of money."<<endl;
        cin>>b[j].money;
+       cout<<"Please enter the type of money."<<endl;
        cin>>b[j].moneytype;
+       cout<<"Please enter the account type."<<endl;
        cin>>b[j].accounttype;
+       cout<<"Please enter the date."<<endl;
        cin>>b[j].date;
        cout<<"CONTINUE or FINISH"<<endl;
        cin>>x;
@@ -50,36 +60,43 @@ main(){
        }  
      }
    cout<<"What do you want to do?"<<endl;
+   cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
    cin>>x;
    }
    if (x=="REPORT"){
      report(a,b,i,j);//Call the report function.
      cout<<"What do you want to do?"<<endl;
+     cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
      cin>>x;
    }  
    if (x=="VIEWINCOME"){
      viewincome(a,max);//Call the viewincome function.
      cout<<"What do you want to do?"<<endl;
+     cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
      cin>>x;
    } 
    if (x=="VIEWEXPENSE"){
      viewexpense(b,max);//Call the viewexpense function.
      cout<<"What do you want to do?"<<endl;
+     cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
      cin>>x;
    }
    if (x=="CURRENCY"){
      currency();//Call the currency function.
      cout<<"What do you want to do?"<<endl;
+     cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
      cin>>x;
    } 
    if (x=="DELETEIN"){
      delete [] a; //This line is to delete income records.
      cout<<"What do you want to do?"<<endl;
+     cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
      cin>>x;
    }
    if (x=="DELETEEX"){
      delete [] b; //This line is to delete expense records.
      cout<<"What do you want to do?"<<endl;
+     cout<<"Please enter: 1.INCOME 2.EXPENSE 3.REPORT 4.VIEWINCOME 5.VIEWEXPENSE 6.CURRENCY 7.DELETEIN 8.DELETEEX 9.DONE"<<endl;
      cin>>x;
    } 
    if (x=="DONE"){
