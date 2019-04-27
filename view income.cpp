4ea071d05e4f20cca957fd6960515146ce3a4ce3
rexwkh <47166231+rexwkh@users.noreply.github.com>
2019-04-27 12:22:55 +0800

    Update view income.cpp

--- view income.cpp
+++ view income.cpp
@@ -1,26 +1,46 @@
-//The user inputs the type of incomes he/she wants to check and then the system outputs the corresponding amount.
+//Function:compute the income of selected type
+//Input:record,selected type
+//Output:total income of selected type
+
 void viewincome(record * a[],int max){
   double sum1=0,sum2=0,sum3=0;
-  string type;
-  cout<<"What type of incomes do you want to check?"<<endl;
-  cin>>type;
-  for(int i=0;i<max;i++){
-    if (a[i].moneytype==type){
-      sum1+=a[i].money;
-    }
-  } 
-  cout<<"You have got "<<sum1<<" from "<<type<<"."<<endl;
-  for(int i=0;i<max;i++){
-    if (a[i].account==type){
-      sum2+=a[i].money;
-    }
-  }
-  cout<<"You have "<<sum2<<" in terms of "<<type<<"."<<endl;
-  for(int i=0;i<max;i++){
-    if (a[i].date==type){
-      sum3+=a[i].money;
-    }
-  }
-  cout<<"You have reveived "<<sum3<<" on "<<type<<"."<<endl;
-    
+  string x;
+  int funct;
+  cout<<"1.View by type 2.View by account 3.View by date"<<endl;
+  cout<<"Please select function."<<endl;
+  cin>>funct;
+  switch(funct){
+    case 1:
+      cout<<"What type of incomes do you want to check?"<<endl;
+      cin>>x;
+      for(int i=0;i<max;i++){
+        if (a[i].moneytype==x){
+          sum1+=a[i].money;
+        }
+      } 
+      cout<<"You have got "<<sum1<<" from "<<type<<"."<<endl;
+      break;
+    case 2:
+      cout<<"Which account do you want to check?"<<endl;
+      cin>>x;
+      for(int i=0;i<max;i++){
+        if (a[i].account==x){
+          sum2+=a[i].money;
+        }
+      }
+      cout<<"You have "<<sum2<<" in terms of "<<type<<"."<<endl;
+      break;
+    case 3:
+      cout<<"Which date do you want to check?"<<endl;
+      cin>>x;
+      for(int i=0;i<max;i++){
+        if (a[i].date==x){
+          sum3+=a[i].money;
+        }
+      }
+      cout<<"You have reveived "<<sum3<<" on "<<type<<"."<<endl;
+      break;
+    default:
+      cout<<"PLease select function 1-3"<<endl;
+  }   
 }
