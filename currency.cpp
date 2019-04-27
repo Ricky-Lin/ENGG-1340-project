9286d5d6615cea9630cce68dd771a52e80ddb10b
rexwkh <47166231+rexwkh@users.noreply.github.com>
2019-04-27 12:42:22 +0800

    Update currency.cpp

--- currency.cpp
+++ currency.cpp
@@ -5,29 +5,35 @@
 void currency(){
   double HKD, changed;
   int currency;
-  cout<<"Please input the amount of HKD and the selected currency type you want to exchange."<<endl;
-  cout<<"1.USD 2.RMB 3.YEN 4.EURO 5.GBP 6.KRW"<<endl;
-  cin>>HKD>>currency;
-  switch(currency){
-    case 1:
-      cout<<"The exchanged money is "<<HKD*0.13<<currency<<"."<<endl;
+  while (true){
+    cout<<"Please input the amount of HKD and the selected currency type you want to exchange."<<endl;
+    cout<<"1.USD 2.RMB 3.YEN 4.EURO 5.GBP 6.KRW"<<endl;
+    cin>>HKD>>currency;
+    switch(currency){
+      case 1:
+        cout<<"The exchanged money is "<<HKD*0.13<<currency<<"."<<endl;
+        break;
+      case 2:
+        cout<<"The exchanged money is "<<HKD*0.86<<currency<<"."<<endl;
+        break;
+      case 3:
+        cout<<"The exchanged money is "<<HKD*14.27<<currency<<"."<<endl;
+        break;
+      case 4:
+        cout<<"The exchanged money is "<<HKD*0.11<<currency<<"."<<endl;
+        break;
+      case 5:
+        cout<<"The exchanged money is "<<HKD*0.098<<currency<<"."<<endl;
+        break;
+      case 6:
+        cout<<"The exchanged money is "<<HKD*145.95<<currency<<"."<<endl;
+        break;
+      default:
+        cout<<"Sorry for unsupported currency."<<endl;
+    }
+    cout<<"1.Continue 2.Exit"<<endl;
+    cin>>currency;
+    if(currency==2){
       break;
-    case 2:
-      cout<<"The exchanged money is "<<HKD*0.86<<currency<<"."<<endl;
-      break;
-    case 3:
-      cout<<"The exchanged money is "<<HKD*14.27<<currency<<"."<<endl;
-      break;
-    case 4:
-      cout<<"The exchanged money is "<<HKD*0.11<<currency<<"."<<endl;
-      break;
-    case 5:
-      cout<<"The exchanged money is "<<HKD*0.098<<currency<<"."<<endl;
-      break;
-    case 6:
-      cout<<"The exchanged money is "<<HKD*145.95<<currency<<"."<<endl;
-      break;
-    default:
-      cout<<"Sorry for unsupported currency."<<endl;
-  }
+    }  
 } 
