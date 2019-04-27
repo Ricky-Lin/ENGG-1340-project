56f98d8337c0852c687300b0fb51eeb8577e833a
rexwkh <47166231+rexwkh@users.noreply.github.com>
2019-04-27 11:31:40 +0800

    Update budget.cpp

--- budget.cpp
+++ budget.cpp
@@ -1,10 +1,12 @@
-// This function is used to issue an warning when user's expenses reach budget.
+// Function:issue an warning when user's expenses reach budget.
+// Input:record,budget
+// Output:warning if users's expenses reach budget
 void budget(record * b, int num,double budget){
   double amount=0;
   for (int i=0;i<=num;i++){
     amount+=b[i].money;
   } 
-  if (amonut>budget){
+  if (amount>budget){
     cout<<"WARNING! YOU HAVE REACHED YOUR BUDGRT. PLEASE MANAGE YOUR WEALTH PROPERLY!!!"<<endl;
   }  
 }  
