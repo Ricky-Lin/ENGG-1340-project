1d0e83bc7f8e0db9f43ec0e1ef70a979f83e35d0
rexwkh <47166231+rexwkh@users.noreply.github.com>
2019-04-27 12:17:46 +0800

    Update view expense.cpp

--- view expense.cpp
+++ view expense.cpp
@@ -1,27 +1,47 @@
-//The user inputs the type of expenses he/she wants to check and then the system outputs the corresponding amount.
+//Function:compute the total expense
+//Input:record,type of expense
+//Output:total expense of selected type
+
 void viewexpense(record * b[],int max){
   double sum1=0,sum2=0,sum3=0;
-  string type;
-  cout<<"What type of expenses do you want to check?"<<endl;
-  cin>>type;
-  for(int i=0;i<max;i++){
-    if (b[i].moneytype==type){
-      sum1+=b[i].money;
-    }
-  } 
-  cout<<"You have spent "<<sum1<<" on "<<type<<"."<<endl;
-  for(int i=0;i<max;i++){
-    if (b[i].account==type){
-      sum2+=b[i].money;
-    }
+  string x;
+  int funct;
+  cout<<"1.View by type 2.View by account 3.View by date"<<endl;
+  cout<<"Please select function."<<endl;
+  cin>>funct;
+  switch(funct){
+    case 1:
+      cout<<"Which type of expenses do you want to check?"<<endl;
+      cin>>x;
+      for(int i=0;i<max;i++){
+        if (b[i].moneytype==x){
+          sum1+=b[i].money;
+        }
+      } 
+      cout<<"You have spent "<<sum1<<" on "<<x<<"."<<endl;
+      break;
+    case 2:
+      cout<<"Which account do you want to check?"<<endl;
+      cin>>x;
+      for(int i=0;i<max;i++){
+        if (b[i].account==x){
+          sum2+=b[i].money;
+        }
+      }
+      cout<<"You have used "<<sum2<<" in terms of "<<x<<"."<<endl;
+      break;
+    case 3:
+      cout<<"Which date do you want to check?"<<endl;
+      cin>>x;
+      for(int i=0;i<max;i++){
+        if (b[i].date==x){
+          sum3+=b[i].money;
+        }
+      }
+      cout<<"You have spent "<<sum3<<" on "<<x<<"."<<endl;
+      break;
+    default:
+      cout<<"PLease select function 1-3"<<endl;
   }
-  cout<<"You have used "<<sum2<<" in terms of "<<type<<"."<<endl;
-  for(int i=0;i<max;i++){
-    if (b[i].date==type){
-      sum3+=b[i].money;
-    }
-  }
-  cout<<"You have spent "<<sum3<<" on "<<type<<"."<<endl;
-    
 }
 
