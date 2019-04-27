9629f3397645cf1df7c9348fb567999d9b373f15
rexwkh <47166231+rexwkh@users.noreply.github.com>
2019-04-27 11:58:03 +0800

    Update budget.cpp

--- budget.cpp
+++ budget.cpp
@@ -1,6 +1,7 @@
 // Function:issue an warning when user's expenses reach budget.
 // Input:record,budget
 // Output:warning if users's expenses reach budget
+
 void budget(record * b, int num,double budget){
   double amount=0;
   for (int i=0;i<=num;i++){
